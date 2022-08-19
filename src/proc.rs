ec2530dc31e4b3d2d932b334711452b023330d6e
dev11n <narcotics@startmail.com>
2022-08-19 18:36:48 +0200

    Use cmd_lib for proc

--- src/proc.rs
+++ src/proc.rs
@@ -1,20 +1,6 @@
-use std::process::Command;
-
-/// Default process to be launched.
-const DEFAULT_PROC: &str = "bash";
-
-/// Default argument to be passed to the process.
-const DEFAULT_ARG: &str = "-c";
+use cmd_lib::run_fun;
 
 /// Executes a command and returns the output.
 pub fn execute(cmd: String) -> String {
-    let output = Command::new(DEFAULT_PROC)
-        .args([DEFAULT_ARG, &cmd])
-        .output()
-        .expect(format!("[ERROR] Failed to execute process '{cmd}'!\n").as_str());
-    let mut result = String::from_utf8_lossy(&output.stdout).to_string();
-
-    // Removes the last character since its an empty line ('\n').
-    result.pop();
-    result
+    run_fun!(/usr/bin/bash -c "$cmd").expect("[ERROR] Failed executing bash command!\n")
 }
