57eabce660f4c7417da20e7e9f695852b2ab91b4
vars1ty <narcotics@startmail.com>
2022-09-13 22:13:27 +0200

    Fix StackOverflow

--- src/environment.rs
+++ src/environment.rs
@@ -1,19 +1,13 @@
 use std::env;
 
-use crate::debug::log;
-
 /// Tries to get the value from a specific environment variable.
 pub fn try_get_var(variable: &str, fallback_value: &str) -> String {
-    {
-        let this = env::var(variable);
-        match this {
-            Ok(t) => t,
-            Err(_) => {
-                log(format!(
-                    "[WARN] Unassigned environment variable '{variable}', using fallback '{fallback_value}'."
-                ));
-                String::from(fallback_value)
-            }
+    let this = env::var(variable);
+    match this {
+        Ok(t) => t,
+        Err(_) => {
+            println!("[HYBRID] [WARN] Unassigned environment variable '{variable}', using fallback '{fallback_value}'.");
+            String::from(fallback_value)
         }
     }
 }
