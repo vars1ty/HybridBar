42e6b7a0bc22d299cc6226208a0913fff2aea76d
vars1ty <narcotics@startmail.com>
2022-09-13 03:26:58 +0200

    Don't use cmd_lib anymore due to crashing with 1 as error codes

--- src/proc.rs
+++ src/proc.rs
@@ -1,6 +1,20 @@
-use cmd_lib::run_fun;
+use std::process::Command;
+
+/// Default process to be launched.
+const DEFAULT_PROC: &str = "bash";
+
+/// Default argument to be passed to the process.
+const DEFAULT_ARG: &str = "-c";
 
 /// Executes a command and returns the output.
 pub fn execute(cmd: String) -> String {
-    run_fun!(/usr/bin/bash -c "$cmd").expect("[ERROR] Failed executing bash command!\n")
+    let output = Command::new(DEFAULT_PROC)
+        .args([DEFAULT_ARG, &cmd])
+        .output()
+        .expect(format!("[ERROR] Failed to execute process '{cmd}'!\n").as_str());
+    let mut result = String::from_utf8_lossy(&output.stdout).to_string();
+
+    // Removes the last character since its an empty line ('\n').
+    result.pop();
+    result
 }
