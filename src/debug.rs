f380955bd85be395dd6207e7653db2b76a82aa68
vars1ty <narcotics@startmail.com>
2022-10-21 17:11:36 +0200

    Remove generics for debug::log

--- src/debug.rs
+++ src/debug.rs
@@ -3,10 +3,7 @@ use std::fmt::*;
 use crate::environment;
 
 /// Logs a message to the terminal if the environment variable `HYBRID_LOG` is set to `1`.
-pub fn log<T>(msg: T)
-where
-    T: Display + Clone + Debug,
-{
+pub fn log(msg: impl Display + Clone + Debug) {
     if environment::try_get_var("HYBRID_LOG", "0") == "1" {
         println!("[HYBRID] [DEBUG] {msg}");
     }
